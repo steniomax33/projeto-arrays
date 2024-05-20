41c63d5196e0dc42158e82340ad2d38d29bac138
Stenio Max <steniomax28@gmail.com>
2024-05-20 12:20:32 -0300

    adicionando array for each

---
+++ scripts.js
@@ -0,0 +1,18 @@
+const list = document.querySelector('ul')
+const buttonShowAll = document.querySelector('.show-all')
+let myLi = ''
+
+function showAll() {
+    menuOptions.forEach(product => {
+        myLi += `
+            <li>
+                <img src=${product.src}>
+                <p>${product.name}</p>
+                <p class="item-price">R$ ${product.price}</p>
+            </li>
+        `
+    })
+    list.innerHTML = myLi
+}
+
+buttonShowAll.addEventListener('click', showAll)
