c4088f348057eda97c8309ab00e35ba63f74b52b
Stenio Max <steniomax28@gmail.com>
2024-05-24 11:26:53 -0300

    foi adicionado o array map

--- scripts.js
+++ scripts.js
@@ -1,18 +1,33 @@
 const list = document.querySelector('ul')
 const buttonShowAll = document.querySelector('.show-all')
-let myLi = ''
+const buttonMapAll = document.querySelector('.map-all')
 
-function showAll() {
-    menuOptions.forEach(product => {
+function showAll(productsArray) {
+    let myLi = ''
+
+
+    productsArray.forEach((product) => {
+
         myLi += `
-            <li>
-                <img src=${product.src}>
-                <p>${product.name}</p>
-                <p class="item-price">R$ ${product.price}</p>
-            </li>
-        `
+                <li>
+                    <img src=${product.src}>
+                    <p>${product.name}</p>
+                    <p class="item-price">R$ ${product.price}</p>
+                </li>
+            `
     })
+
+
     list.innerHTML = myLi
 }
 
-buttonShowAll.addEventListener('click', showAll)
+function mapAllItems() {
+    const newPrice = menuOptions.map((product) => ({
+        ...product, // Spread Operador
+        price: product.price * 0.9, //dar 10% de desconto
+    }))
+    showAll(newPrice)
+}
+
+buttonShowAll.addEventListener('click', () => showAll(menuOptions))
+buttonMapAll.addEventListener('click', mapAllItems)
